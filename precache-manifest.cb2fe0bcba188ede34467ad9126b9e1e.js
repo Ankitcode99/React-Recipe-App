3693ad47fb0aef221581211ca02591e20d6329b1
AnkitCode99 <9999385728ankit@gmail.com>
2020-07-16 03:13:40 +0530

    Updates

---
+++ precache-manifest.cb2fe0bcba188ede34467ad9126b9e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43267feb4d08e95e5499b193d3193ec2",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "add615c05a9ec485bba0",
+    "url": "/React-Recipe-App/static/css/main.e0303ea9.chunk.css"
+  },
+  {
+    "revision": "0ec735653251a014fa5c",
+    "url": "/React-Recipe-App/static/js/2.99ef12dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe-App/static/js/2.99ef12dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add615c05a9ec485bba0",
+    "url": "/React-Recipe-App/static/js/main.62babe88.chunk.js"
+  },
+  {
+    "revision": "005a562e3306d8ddfa27",
+    "url": "/React-Recipe-App/static/js/runtime-main.98c59965.js"
+  }
+]);
